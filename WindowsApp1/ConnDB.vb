1b8eac71be3d6abd3ab344ac0351359febd275de
Mochamad Bachtiar <mobch06@gmail.com>
2021-05-23 17:47:38 +0700

    Mengganti koneksi database dan auto sum pesanan per menu

--- WindowsApp1/ConnDB.vb
+++ WindowsApp1/ConnDB.vb
@@ -6,10 +6,11 @@ Module ConnDB
     Public cmd As OdbcCommand
     Public strConn As String = "server=localhost; user id=root; password=; database=dbcafeapp; dsn=dbcafe"
     Public strcon As String = "server=mysql5045.site4now.net; user id=a74197_dbcafe password=killerbee; database=db_a74197_dbcafe; Dsn=dbcafeonline"
+    Public strcon2 As String = "server=sgx02.dewaweb.com; user id=tuhfahst_admin_cafe password=Killerbee12!; database=tuhfahst_db_cafe; Dsn=dbcafeonline2"
 
     Public Sub openConn()
         If conn.State = ConnectionState.Closed Then
-            conn.ConnectionString = strcon
+            conn.ConnectionString = strcon2
             Try
                 conn.Open()
             Catch ex As OdbcException
